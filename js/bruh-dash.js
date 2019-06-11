7d189e9b3e03eed86b352bc2f37d789bb6b3b3c2
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-06-10 22:00:57 -1000

    finished up to drop

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -10,43 +10,84 @@ var global = window || GLOBAL;
 global.bruhdash = {
 
   // returns the first element of an array
-  first: function () {
-      
+  first: function (arr) {
+    return arr[0];
   },
 
   // returns the last element of an array
-  last: function () {
-
+  last: function (arr) {
+    return arr[arr.length-1];
   },
 
   // returns the index of the first matching element from left to right
-  indexOf: function () {
-
+  indexOf: function (arr,value) {
+    for (i=0;i<arr.length;i++){
+      if (arr[i]===value){
+        return i;
+      }
+    }
   },
 
   // returns the index of the first matching element from right to left
-  lastIndexOf: function () {
-
+  lastIndexOf: function (arr, value) {
+    for (i=arr.length-1;i>=0;i--){
+      if (arr[i]===value){
+        return i;
+      }
+    }return -1;
   },
 
   // returns an array with all elements except for the last element
-  initial: function () {
-
+  initial: function (arr) {
+    newArr = [];
+    for (i=0;i<arr.length-1;i++){
+      newArr[i] = arr[i];
+    }
+    return newArr;
   },
   
   // returns an array with all falsey values removed
-  compact: function() {
-
+  compact: function(arr) {
+    // console.log(arr);
+    // console.log(typeof arr[5]);
+    var newArr = [];
+    for (i=0;i<arr.length;i++){
+      if (arr[i]){
+        newArr[newArr.length] = arr[i]
+      }
+    }
+    return newArr;
   },
 
   // creates a slice of an array from the start index up to but not including the end index
-  slice: function () {
-
+  slice: function (arr, index1,index2) {
+    newArr = [];
+    // console.log (arr);
+    // console.log (index1);
+    // console.log (index2);
+    for (i=index1;i<index2;i++){
+        newArr[newArr.length] = arr[i];
+      }
+    return newArr;
   },
 
   // returns a slice of array with n elements dropped from the beignning
-  drop: function(){
+  drop: function(arr, n){
+    newArr = [];
+    console.log(arr);
+    console.log(n);
+    if (n === undefined){
+      for (i=1;i<arr.length;i++){
+        newArr[newArr.length] = arr[i];
+      }
+    }else{
+      for (i=n;i<arr.length;i++){
+        newArr[newArr.length] = arr[i];
+      }
+  } 
+    console.log('this is the new arr' +newArr);
 
+    return newArr;
   },
 
   // returns a slice of array with n elements dropped from the end
