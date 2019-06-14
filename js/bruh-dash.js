d491e43952ee140a04e7c77bf42f118f30406a26
Daniel Ichiyama <daniel.ichiyama@gmail.com>
2019-06-14 08:45:11 -1000

    started on forEach

--- js/bruh-dash.js
+++ js/bruh-dash.js
@@ -384,8 +384,8 @@ global.bruhdash = {
 
   // iterates over elements of a collection and invokes iteratee for each element
   // Note: this should work for arrays and objects
-  forEach: function() {
-
+  forEach: function(coll, iteratee) {
+    console.log(arguments);
   },
 
   // creates an array of values by running each element in collection thru the iteratee
